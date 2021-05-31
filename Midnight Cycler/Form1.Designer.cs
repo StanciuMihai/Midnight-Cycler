6e0def64bef38ca909adea436ad7dec4e7290b04
Stanciu Mihai <misu_sergiu@yahoo.com>
2021-06-01 00:01:31 +0300

    Completed game graphics

--- Midnight Cycler/Form1.Designer.cs
+++ Midnight Cycler/Form1.Designer.cs
@@ -29,14 +29,28 @@ namespace Midnight_Cycler
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.base_1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pipe_1 = new System.Windows.Forms.PictureBox();
+            this.pipe_2 = new System.Windows.Forms.PictureBox();
+            this.pipe_3 = new System.Windows.Forms.PictureBox();
+            this.base_2 = new System.Windows.Forms.PictureBox();
+            this.base_3 = new System.Windows.Forms.PictureBox();
+            this.base_4 = new System.Windows.Forms.PictureBox();
+            this.pipe_5 = new System.Windows.Forms.PictureBox();
+            this.pipe_4 = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.Point = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.base_1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipe_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipe_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipe_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.base_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.base_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.base_4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipe_5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipe_4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
             this.SuspendLayout();
             // 
             // base_1
@@ -49,58 +63,146 @@ namespace Midnight_Cycler
             this.base_1.TabIndex = 0;
             this.base_1.TabStop = false;
             // 
-            // pictureBox1
+            // pipe_1
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.pictureBox1.Location = new System.Drawing.Point(12, 293);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(12, 50);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.pipe_1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.pipe_1.Location = new System.Drawing.Point(12, 305);
+            this.pipe_1.Name = "pipe_1";
+            this.pipe_1.Size = new System.Drawing.Size(12, 38);
+            this.pipe_1.TabIndex = 1;
+            this.pipe_1.TabStop = false;
             // 
-            // pictureBox2
+            // pipe_2
             // 
-            this.pictureBox2.BackColor = System.Drawing.Color.Yellow;
-            this.pictureBox2.Location = new System.Drawing.Point(372, 293);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(12, 50);
-            this.pictureBox2.TabIndex = 2;
-            this.pictureBox2.TabStop = false;
+            this.pipe_2.BackColor = System.Drawing.Color.Yellow;
+            this.pipe_2.Location = new System.Drawing.Point(372, 317);
+            this.pipe_2.Name = "pipe_2";
+            this.pipe_2.Size = new System.Drawing.Size(12, 26);
+            this.pipe_2.TabIndex = 2;
+            this.pipe_2.TabStop = false;
             // 
-            // pictureBox3
+            // pipe_3
             // 
-            this.pictureBox3.BackColor = System.Drawing.Color.Fuchsia;
-            this.pictureBox3.Location = new System.Drawing.Point(738, 293);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(12, 50);
-            this.pictureBox3.TabIndex = 3;
-            this.pictureBox3.TabStop = false;
+            this.pipe_3.BackColor = System.Drawing.Color.Fuchsia;
+            this.pipe_3.Location = new System.Drawing.Point(738, 293);
+            this.pipe_3.Name = "pipe_3";
+            this.pipe_3.Size = new System.Drawing.Size(12, 50);
+            this.pipe_3.TabIndex = 3;
+            this.pipe_3.TabStop = false;
+            // 
+            // base_2
+            // 
+            this.base_2.BackColor = System.Drawing.Color.Silver;
+            this.base_2.Location = new System.Drawing.Point(12, 135);
+            this.base_2.Name = "base_2";
+            this.base_2.Size = new System.Drawing.Size(150, 10);
+            this.base_2.TabIndex = 4;
+            this.base_2.TabStop = false;
+            // 
+            // base_3
+            // 
+            this.base_3.BackColor = System.Drawing.Color.Silver;
+            this.base_3.Location = new System.Drawing.Point(308, 182);
+            this.base_3.Name = "base_3";
+            this.base_3.Size = new System.Drawing.Size(150, 10);
+            this.base_3.TabIndex = 5;
+            this.base_3.TabStop = false;
+            // 
+            // base_4
+            // 
+            this.base_4.BackColor = System.Drawing.Color.Silver;
+            this.base_4.Location = new System.Drawing.Point(600, 223);
+            this.base_4.Name = "base_4";
+            this.base_4.Size = new System.Drawing.Size(150, 10);
+            this.base_4.TabIndex = 6;
+            this.base_4.TabStop = false;
+            // 
+            // pipe_5
+            // 
+            this.pipe_5.BackColor = System.Drawing.Color.Blue;
+            this.pipe_5.Location = new System.Drawing.Point(650, 135);
+            this.pipe_5.Name = "pipe_5";
+            this.pipe_5.Size = new System.Drawing.Size(100, 10);
+            this.pipe_5.TabIndex = 7;
+            this.pipe_5.TabStop = false;
+            // 
+            // pipe_4
+            // 
+            this.pipe_4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.pipe_4.Location = new System.Drawing.Point(12, 182);
+            this.pipe_4.Name = "pipe_4";
+            this.pipe_4.Size = new System.Drawing.Size(100, 10);
+            this.pipe_4.TabIndex = 8;
+            this.pipe_4.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.Image = ((System.Drawing.Image)(resources.GetObject("player.Image")));
+            this.player.Location = new System.Drawing.Point(397, 317);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(41, 26);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 9;
+            this.player.TabStop = false;
+            // 
+            // Point
+            // 
+            this.Point.AutoSize = true;
+            this.Point.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Point.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.Point.Location = new System.Drawing.Point(372, 13);
+            this.Point.Name = "Point";
+            this.Point.Size = new System.Drawing.Size(66, 16);
+            this.Point.TabIndex = 10;
+            this.Point.Text = "Score -0";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(784, 411);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Point);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.pipe_4);
+            this.Controls.Add(this.pipe_5);
+            this.Controls.Add(this.base_4);
+            this.Controls.Add(this.base_3);
+            this.Controls.Add(this.base_2);
+            this.Controls.Add(this.pipe_3);
+            this.Controls.Add(this.pipe_2);
+            this.Controls.Add(this.pipe_1);
             this.Controls.Add(this.base_1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Midnight Cycler";
             ((System.ComponentModel.ISupportInitialize)(this.base_1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipe_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipe_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipe_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.base_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.base_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.base_4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipe_5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipe_4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox base_1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pipe_1;
+        private System.Windows.Forms.PictureBox pipe_2;
+        private System.Windows.Forms.PictureBox pipe_3;
+        private System.Windows.Forms.PictureBox base_2;
+        private System.Windows.Forms.PictureBox base_3;
+        private System.Windows.Forms.PictureBox base_4;
+        private System.Windows.Forms.PictureBox pipe_5;
+        private System.Windows.Forms.PictureBox pipe_4;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Label Point;
     }
 }
 
